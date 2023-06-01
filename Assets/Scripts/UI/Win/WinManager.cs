f4ece12195d5b2564fd8c17e610891037f6709e8
Krebl <kreb.lak@mail.ru>
2023-06-01 23:12:11 +0300

    Set reference in ui

--- Assets/Scripts/UI/Win/WinManager.cs
+++ Assets/Scripts/UI/Win/WinManager.cs
@@ -1,12 +1,20 @@
+using System;
 using Game.Production.Tools;
+using UniRx;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Game.Production.UI
 {
     internal class WinManager : BaseDisposable
     {
+        private const string PREFAB_UI = "Prefabs/WinView";
+        
         public struct Ctx
         {
-            
+            public IResourceLoader resourceLoader;
+            public Transform uiContainer;
+            public Action returnToMain;
         }
 
         private readonly Ctx _ctx;
@@ -14,6 +22,20 @@ namespace Game.Production.UI
         public WinManager(Ctx ctx)
         {
             _ctx = ctx;
+            LoadOnScene();
+        }
+
+        private void LoadOnScene()
+        {
+            GameObject prefab = _ctx.resourceLoader.LoadPrefab(PREFAB_UI);
+            GameObject objOnScene = AddComponent(Object.Instantiate(prefab, _ctx.uiContainer, false));
+            WinView view = objOnScene.GetComponent<WinView>();
+            CompositeDisposable viewDisposable = AddDispose(new CompositeDisposable());
+            view.SetCtx(new WinView.Ctx
+            {
+                viewDisposable = viewDisposable,
+                returnToMain = _ctx.returnToMain,
+            });
         }
     }
 }
